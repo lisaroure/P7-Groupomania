a5d2a5be674265a95d1242e8f6bb1aa462eb3ae8
Lili <lisaroure@gmail.com>
2022-10-03 21:54:09 +0200

    corrections mineures

--- groupomanie/backend/models/Post.js
+++ groupomanie/backend/models/Post.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    message: { type: String, trim: true, maxlength: 500 },
-    imageUrl: { type: String, required: true },
-    likes: { type: Number, default: 0 },
-    dislikes: { type: Number, default: 0 },
-    usersLiked: { type: [String] },
-    usersDisliked: { type: [String] },
-    comments: {
-        type: [{ commenterId: String, commenterPseudo: String, text: String, timestamp: Number }],
-        required: true,
+    posterId: {
+        type: String,
+        required: true
     },
+    post: {
+        type: String,
+        maxlength: true,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+    },
+    likers: {
+        type: [String]
+    },
+
 },
     {
         timestamps: true,
