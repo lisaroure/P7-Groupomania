d3fa3c26201707661fb5c06121c020749ec4c90f
Lili <lisaroure@gmail.com>
2022-10-03 21:57:08 +0200

    corrections

--- groupomanie/backend/models/User.js
+++ groupomanie/backend/models/User.js
@@ -3,12 +3,25 @@ const uniqueValidator = require('mongoose-unique-validator');
 const { isEmail } = require('validator');
 
 const userSchema = new mongoose.Schema({
-    pseudo: { type: String, minlenght: 3, maxlenght: 55, required: true, unique: true, trim: true },
-    email: { type: String, required: true, validate: [isEmail], lowercase: true, unique: true, trim: true },
-    password: { type: String, required: true, max: 1024, minlenght: 8, maxlenght: 10 },
-    picture: { type: String, default: "./uploads/profil/random-user.png" },
-    bio: { type: String, max: 1024 },
-
+    pseudo: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email: {
+        type: String,
+        validate: [isEmail],
+        lowercase: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: [String]
+    }
 },
     {
         timestamps: true,
