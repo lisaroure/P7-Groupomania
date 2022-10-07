03e8f193f57caca5a8464f4fd630c2119f3e5f6f
Lili <lisaroure@gmail.com>
2022-10-07 11:11:00 +0200

    fermture admin

--- groupomanie/src/App.js
+++ groupomanie/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdminRouter from './pages/Admin/AdminRouter';
 import AuthRouter from './pages/Auth/AuthRouter';
 import PublicRouter from './pages/Public/PublicRouter';
+import AuthGuard from './_helpers/AuthGuard';
 
 const App = () => {
     return (
@@ -10,7 +11,11 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path='/*' element={<PublicRouter />} />
-                    <Route path='/admin/*' element={< AdminRouter />} />
+                    <Route path='/admin/*' element={
+                        <AuthGuard>
+                            < AdminRouter />
+                        </AuthGuard>
+                    } />
                     <Route path='/auth/*' element={<AuthRouter />} />
                 </Routes>
             </BrowserRouter>
