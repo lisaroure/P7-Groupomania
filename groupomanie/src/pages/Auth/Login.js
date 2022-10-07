ea7a399654f70770acf171b9f44c18a50f2c83cd
Lili <lisaroure@gmail.com>
2022-10-07 11:24:47 +0200

    edit form state

--- groupomanie/src/pages/Auth/Login.js
+++ groupomanie/src/pages/Auth/Login.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './auth.scss';
 
 const Login = () => {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log('form');
+    }
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className='group'>
                 <label htmlFor='login'>Identifiant</label>
-                <input type="text" name='login' />
+                <input
+                    type="text"
+                    name='login'
+                    value={login}
+                    onChange={e => setLogin(e.target.value)}
+                />
             </div>
             <div className='group'>
                 <label htmlFor='password'>Mot de passe</label>
-                <input type="text" name='password' />
+                <input
+                    type="text"
+                    name='password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
             </div>
             <div className='group'>
                 <button>Connexion</button>
