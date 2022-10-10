b975e0c546d1b08116cb7aa99fe7ec4462640766
Lisa <lisaroure@gmail.com>
2022-10-10 08:40:35 +0200

    get admin

--- groupomanie/src/pages/Admin/AdminRouter.js
+++ groupomanie/src/pages/Admin/AdminRouter.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import { ALayout, Dashboard } from '../Admin';
-import { User, UEdit, Add } from '../Admin/User';
-import { Post, PEdit } from '../Admin/Post';
-import Error from '../../_utils/Error';
+import { ALayout, Dashboard } from "../Admin";
+import { User, UEdit, Add, Admin } from "../Admin/User";
+import { Post, PEdit } from "../Admin/Post";
+import Error from "../../_utils/Error";
 
 const AdminRouter = () => {
-    return (
-        <Routes>
-            <Route element={<ALayout />}>
-                <Route index element={<Dashboard />} />
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="user">
-                    <Route path="index" element={<User />} />
-                    <Route path="edit/:uid" element={<UEdit />} />
-                    <Route path="add" element={<Add />} />
-                </Route>
-                <Route path="post">
-                    <Route path="index" element={<Post />} />
-                    <Route path="edit" element={<PEdit />} />
-                </Route>
-                <Route path='*' element={<Error />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route element={<ALayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="admin">
+          <Route path="admin" element={<Admin />} />
+          <Route path="edit/:uid" element={<UEdit />} />
+          <Route path="add" element={<Add />} />
+        </Route>
+        <Route path="user">
+          <Route path="index" element={<User />} />
+          <Route path="edit/:uid" element={<UEdit />} />
+          <Route path="add" element={<Add />} />
+        </Route>
+        <Route path="post">
+          <Route path="index" element={<Post />} />
+          <Route path="edit" element={<PEdit />} />
+        </Route>
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default AdminRouter;
