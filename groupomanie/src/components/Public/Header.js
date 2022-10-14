d9898be3a0e08dd5b620534bf425a7f68af30798
Lisa <lisaroure@gmail.com>
2022-10-14 11:53:34 +0200

    edit style

--- groupomanie/src/components/Public/Header.js
+++ groupomanie/src/components/Public/Header.js
@@ -1,27 +1,31 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import logo from '../../assets/logo.png'
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../assets/logo.png";
 import { accountService } from "../../_services/account.service";
 
 import "./header.scss";
 
 const Header = () => {
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
   const logout = () => {
-    accountService.logout()
-    navigate('/home')
-  }
+    accountService.logout();
+    navigate("/home");
+  };
 
   return (
     <>
       <header className="home-header">
-        <img
-          src={logo}
-          alt='logo de Groupomania'
-          width={130}
-          height={130}
-        />
+        <div className="img-header">
+          <Link to="/home">
+            <img
+              src={logo}
+              alt="logo de Groupomania"
+              width={130}
+              height={130}
+            />
+          </Link>
+        </div>
         <button onClick={logout}>Se déconnecter</button>
       </header>
     </>
--- groupomanie/src/pages/Public/Home.js
+++ groupomanie/src/pages/Public/Home.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import './home.scss'
-import logo from '../../assets/groupomania.jpg'
+import React from "react";
+import "./home.scss";
+import logo from "../../assets/groupomania.jpg";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  return (
+    <div className="home-container">
+      <div>
+        <h2>Bienvenue sur le réseau social de Groupomania !</h2>
+        <p>Cliquez sur le logo pour accéder au site !</p>
+      </div>
 
-    return (
-        <div className='home-container'>
-
-            <h2>Bienvenue sur le réseau social de Groupomania.</h2>
-            <img
-                src={logo}
-                alt='logo de groupomania'
-                width={270}
-            />
-
-        </div>
-    );
+      <Link to="auth/login">
+        <img src={logo} alt="logo de groupomania" width={270} />
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
