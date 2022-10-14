60f6c044b3579384621463d16979af6621b01517
Lisa <lisaroure@gmail.com>
2022-10-14 11:26:27 +0200

    edit styles

--- groupomanie/src/pages/Auth/Login.js
+++ groupomanie/src/pages/Auth/Login.js
@@ -1,67 +1,70 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { accountService } from '../../_services/account.service'
-import logo from '../../assets/groupomania.jpg'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { accountService } from "../../_services/account.service";
+import logo from "../../assets/groupomania.jpg";
 
-import './auth.scss';
+import "./auth.scss";
 
 const Login = () => {
-    let navigate = useNavigate()
+  let navigate = useNavigate();
 
-    // initialisation du state
-    const [credentials, setCredentials] = useState({
-        email: '',
-        password: ''
-    })
-    //passer par l'état précédent pour modifier les champs input
-    const onChange = (e) => {
-        setCredentials({
-            ...credentials,
-            [e.target.name]: e.target.value
-        })
-    }
-    //déclenchement du formulaire
-    const onSubmit = (e) => {
-        e.preventDefault()
-        accountService.login(credentials)
-            .then(res => {
-                accountService.saveToken(res.data.token)
-                navigate('/accueil')
-            })
-            .catch(error => console.log(error))
-    }
+  // initialisation du state
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  //passer par l'état précédent pour modifier les champs input
+  const onChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //déclenchement du formulaire
+  const onSubmit = (e) => {
+    e.preventDefault();
+    accountService
+      .login(credentials)
+      .then((res) => {
+        accountService.saveToken(res.data.token);
+        navigate("/accueil");
+      })
+      .catch((error) => console.log(error));
+  };
 
-    return (
-        <form onSubmit={onSubmit}>
-            <img src={logo} alt='logo de Groupomania'/>
-            <h3>Connexion</h3>
-            <div className='group'>
-                <label htmlFor='email'>Adresse e-mail</label>
-                <input
-                    type="text"
-                    name='email'
-                    placeholder='Votre e-mail'
-                    value={credentials.email}
-                    onChange={onChange}
-                />
-            </div>
-            <div className='group'>
-                <label htmlFor='password'>Mot de passe</label>
-                <input
-                    type="text"
-                    name='password'
-                    placeholder='Votre mot de passe'
-                    value={credentials.password}
-                    onChange={onChange}
-                />
-            </div>
-            <div className='group'>
-                <button>Connexion</button>
-            </div>
-                <p>Pas de compte ?</p>
-                <Link to='/auth/signup'>Inscrivez vous !</Link>
-        </form>
-    );
+  return (
+    <form onSubmit={onSubmit}>
+      <img src={logo} alt="logo de Groupomania" />
+      <h3>Connexion</h3>
+      <div className="group">
+        <label htmlFor="email">Adresse e-mail</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Votre e-mail"
+          value={credentials.email}
+          onChange={onChange}
+        />
+      </div>
+      <div className="group">
+        <label htmlFor="password">Mot de passe</label>
+        <input
+          type="text"
+          name="password"
+          placeholder="Votre mot de passe"
+          value={credentials.password}
+          onChange={onChange}
+        />
+      </div>
+      <div className="group">
+        <button>Connexion</button>
+      </div>
+      <div className="suggestion">
+        <p>Pas de compte ?</p>
+        <Link to="/auth/signup">Inscrivez vous !</Link>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
--- groupomanie/src/pages/Auth/Signup.js
+++ groupomanie/src/pages/Auth/Signup.js
@@ -1,80 +1,81 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { accountService } from '../../_services/account.service'
-import logo from '../../assets/groupomania.jpg'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { accountService } from "../../_services/account.service";
+import logo from "../../assets/groupomania.jpg";
 
-import './auth.scss';
+import "./auth.scss";
 
 const Signup = () => {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate()
+  // initialisation du state
+  const [credentials, setCredentials] = useState({
+    pseudo: "",
+    email: "",
+    password: "",
+  });
+  //passer par l'état précédent pour modifier les champs input
+  const onChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+  //déclenchement du formulaire
+  const onSubmit = (e) => {
+    e.preventDefault();
+    accountService
+      .signup(credentials)
+      .then((res) => {
+        accountService.saveToken(res.data.token);
+        navigate("/accueil");
+      })
+      .catch((error) => console.log(error));
+  };
 
-    // initialisation du state
-    const [credentials, setCredentials] = useState({
-        pseudo: '',
-        email: '',
-        password: ''
-    })
-    //passer par l'état précédent pour modifier les champs input
-    const onChange = (e) => {
-        setCredentials({
-            ...credentials,
-            [e.target.name]: e.target.value
-        })
-    }
-    //déclenchement du formulaire
-    const onSubmit = (e) => {
-        e.preventDefault()
-        accountService.signup(credentials)
-            .then(res => {
-                accountService.saveToken(res.data.token)
-                navigate('/accueil')
-            })
-            .catch(error => console.log(error))
-    }
-
-
-    return (
-        <form onSubmit={onSubmit}>
-            <img src={logo} alt='logo de groupomania' />
-            <h3>Inscription</h3>
-            <div className='group'>
-                <label htmlFor='pseudo'>Pseudo</label>
-                <input
-                    type="text"
-                    name='pseudo'
-                    placeholder='Votre pseudo'
-                    value={credentials.pseudo}
-                    onChange={onChange}
-                />
-            </div>
-            <div className='group'>
-                <label htmlFor='email'>Adresse e-mail</label>
-                <input
-                    type="text"
-                    name='email'
-                    placeholder='Votre e-mail'
-                    value={credentials.email}
-                    onChange={onChange}
-                />
-            </div>
-            <div className='group'>
-                <label htmlFor='password'>Mot de passe</label>
-                <input
-                    type="text"
-                    name='password'
-                    placeholder='Votre mot de passe'
-                    value={credentials.password}
-                    onChange={onChange}
-                />
-            </div>
-            <div className='group'>
-                <button>Créer un compte</button>
-            </div>
-                <p>Déjà inscrit ?</p>
-                <Link to='/auth/login'>Connectez vous !</Link>
-        </form >
-    );
+  return (
+    <form onSubmit={onSubmit}>
+      <img src={logo} alt="logo de groupomania" />
+      <h3>Inscription</h3>
+      <div className="group">
+        <label htmlFor="pseudo">Pseudo</label>
+        <input
+          type="text"
+          name="pseudo"
+          placeholder="Votre pseudo"
+          value={credentials.pseudo}
+          onChange={onChange}
+        />
+      </div>
+      <div className="group">
+        <label htmlFor="email">Adresse e-mail</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="Votre e-mail"
+          value={credentials.email}
+          onChange={onChange}
+        />
+      </div>
+      <div className="group">
+        <label htmlFor="password">Mot de passe</label>
+        <input
+          type="text"
+          name="password"
+          placeholder="Votre mot de passe"
+          value={credentials.password}
+          onChange={onChange}
+        />
+      </div>
+      <div className="group">
+        <button>Créer un compte</button>
+      </div>
+      <div className="suggestion">
+        <p>Déjà inscrit ?</p>
+        <Link to="/auth/login">Connectez vous !</Link>
+      </div>
+    </form>
+  );
 };
 
 export default Signup;
