0d94ee7aede5600bc9dc57d0819e0403ac2a1dbf
Lisa <lisaroure@gmail.com>
2022-10-17 09:37:41 +0200

    edits

--- groupomanie/src/pages/Home.js
+++ groupomanie/src/pages/Home.js
@@ -1,17 +1,14 @@
 import React from "react";
 import "./home.scss";
-import logo from "../../assets/groupomania.jpg";
-import { Link } from "react-router-dom";
+import logo from "../assets/groupomania.jpg";
+import Accueil from "./Posts/Accueil";
 
 const Home = () => {
-
-  
-
   return (
     <div className="home-container">
       <h2>Bienvenue sur le réseau social de Groupomania !</h2>
-      <p>Accéder à <Link to="/accueil">mon compte</Link></p>
       <img src={logo} alt="logo de groupomania" />
+      <Accueil />
     </div>
   );
 };
